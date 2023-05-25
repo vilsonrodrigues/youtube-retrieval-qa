4efdea676e1e01b27553a815f34aa40486ec3f38
vilson <vilsonrodrigues07@hotmail.com>
2023-05-25 01:35:52 -0300

    fix: switch NoReturn to None

--- qa/manager.py
+++ qa/manager.py
@@ -1,5 +1,3 @@
-from typing import NoReturn
-
 from chains import retrieval_qa
 from loader import youtube_doc_loader
 from model import load_llm
@@ -11,15 +9,15 @@ class QA:
     def __init__(self):
         pass
 
-    def load_model(self) -> NoReturn:
+    def load_model(self) -> None:
         self.llm = load_llm()
 
-    def load_vector_store(self, url: str) -> NoReturn:
+    def load_vector_store(self, url: str) -> None:
         data = youtube_doc_loader(url=url)
         docs = split_document(data=data)
         self.retriver = create_vector_store(docs=docs)
      
-    def load_retriever(self) -> NoReturn:
+    def load_retriever(self) -> None:
         self.retrieval_qa = retrieval_qa(llm=self.llm, retriever=self.retriver)
 
     def run(self, question: str) -> str:
