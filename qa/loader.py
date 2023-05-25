ea3851e0ff961a4145f8c786cfd3c85e09a03284
vilson <vilsonrodrigues07@hotmail.com>
2023-05-25 01:15:29 -0300

    fix: add List typing hint import

--- qa/loader.py
+++ qa/loader.py
@@ -1,3 +1,5 @@
+from typing import List
+
 def youtube_doc_loader(url: str) -> List:
     from langchain.document_loaders import YoutubeLoader
     loader = YoutubeLoader.from_youtube_url(url, add_video_info=False)
