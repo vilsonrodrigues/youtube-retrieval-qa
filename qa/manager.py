e24e56b7e7b1e4910503300aae008f0fdcebe61c
vilson <vilsonrodrigues07@hotmail.com>
2023-05-25 01:32:07 -0300

    fix: remove NoReturn hint in __ini__

--- qa/manager.py
+++ qa/manager.py
@@ -8,13 +8,13 @@ from vector_store import create_vector_store
 
 class QA:
 
-    def __init__(self) -> NoReturn:
+    def __init__(self):
         pass
 
     def load_model(self) -> NoReturn:
         self.llm = load_llm()
 
-    def load_vector_store(self, url: str)  -> NoReturn:
+    def load_vector_store(self, url: str) -> NoReturn:
         data = youtube_doc_loader(url=url)
         docs = split_document(data=data)
         self.retriver = create_vector_store(docs=docs)
