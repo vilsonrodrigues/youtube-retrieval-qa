8bc25cf84080fe505d29a696dc97fb3d40e1bb73
vilson <vilsonrodrigues07@hotmail.com>
2023-05-30 18:30:14 -0300

    Add: conversational retrieval chain

--- qa/chains.py
+++ qa/chains.py
@@ -4,3 +4,14 @@ def retrieval_qa(llm: Callable, retriever: Callable) -> Callable:
     from langchain.chains import RetrievalQA
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
     return qa
+
+def conversational_retrieval_qa(llm: Callable, retriever: Callable) -> Callable: 
+    from langchain.memory import ConversationBufferMemory
+    from langchain.chains import ConversationalRetrievalChain
+    memory = ConversationBufferMemory(memory_key="chat_history", 
+                                  return_messages=True)
+    qa = ConversationalRetrievalChain.from_llm(
+            llm, 
+            retriever=retriever, 
+            memory=memory)    
+    return qa
